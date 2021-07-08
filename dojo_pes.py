54374ae8a2d273d4a3c08d5f7dfd54636f75ad84
somir1 <leonsomir@gmail.com>
2021-07-08 11:56:18 -0500

    starting of dojo pets

---
+++ dojo_pes.py
@@ -0,0 +1,28 @@
+class Ninja:
+
+    # implment attributes
+    def __init__(self, first_name, last_name, treats, pet_food, pet):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.treats = treats
+        self.petfood = pet_food
+        self.pet = pet
+
+    # methods
+    def walk():
+    def feed():
+    def bathe():
+
+
+class Pet:
+    def __init__(self, name, type, tricks, health, energy):
+        self.name = name
+        self.type = type
+        self.tricks = tricks
+        self.health = health
+        self.energy = energy
+
+    def sleep():
+    def eat():
+    def play():
+    def noise():
